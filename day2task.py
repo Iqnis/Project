7b4cd187067bfd5798fe6d2508abc7603244add6
Muhammad Iqbal bin Rosman <174858783+Iqnis@users.noreply.github.com>
2024-08-23 11:52:50 +0800

    Add files via upload

---
+++ day2task.py
@@ -0,0 +1,37 @@
+# Q1
+# def greet (name, greet = 'Hello'):
+#   print (f'{greet} {name}')
+# greet ('Nazca')
+
+# Q2
+# def greet(firstname, lastname):
+#   print(f'Well Hello {firstname} {lastname}!')
+# firstname = input('Enter your firstname: ')
+# lastname = input('Enter your lastname: ')
+# greet(firstname, lastname)
+
+# Q3
+# def numbah (a,b):
+#  print (a*b)
+# numbah (1,3)
+
+# Q4
+# fruits = ['apple', 'banana', 'cherry', 'date']
+# fruits.append ('elderberries')
+# fruits.remove ('banana')
+# fruits.insert (1, 'blueberries')
+# print (fruits)
+
+# Q5
+# for n in range (1,11):
+#   print (n)
+#   if n == 7:
+#     print('FINAL ATTACK! LOOP BREAK!!!')
+#     break
+
+# Q6
+# for n in range(1, 11):
+#   if n % 3 == 0:
+#     continue
+#   print(n)
+# print('No more number multiple by 3')
